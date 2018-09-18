efe89fc7441dee1b069ef897ef5f32a937441bc9
cpage-pivotal <cpage@pivotal.io>
2018-09-18 13:31:25 -0500

    Added cleanup and faststream

---
+++ scripts/cleanup.sh
@@ -0,0 +1,6 @@
+riff service delete generator
+riff service delete powerof2
+riff service delete textdisplay
+riff channel delete numbers
+riff channel delete squares
+
--- scripts/faststream.sh
+++ scripts/faststream.sh
@@ -1,3 +1,3 @@
 riff service invoke generator -- -w '\n' \
   -H 'Content-Type:application/json' \
-  -d '{"url":"http://numbers-channel.default.svc.cluster.local"}'
+  -d '{"url":"http://numbers-channel.default.svc.cluster.local","freq":200}'
