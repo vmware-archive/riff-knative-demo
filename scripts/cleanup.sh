455e5a2ba1d990ec93e28eff3cc9c36cb88bebff
cpage-pivotal <cpage@pivotal.io>
2018-09-18 14:09:39 -0500

    Added scale image

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,4 +3,5 @@ riff service delete powerof2
 riff service delete textdisplay
 riff channel delete numbers
 riff channel delete squares
-
+kubectl delete subscription powerof2
+kubectl delete subscription textdisplay
